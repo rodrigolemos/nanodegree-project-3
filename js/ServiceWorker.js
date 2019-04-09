a70d990dc26a3fd95492a3c6cfcad41931405e46
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2019-04-09 09:27:51 -0300

    Update ServiceWorker.js

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -1,9 +1,12 @@
+/**
+ * sw register
+ */
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('sw.js').then(function (register) {
-        console.log('ServiceWorker registered!');
-      }, function (err) {
-        console.log('ServiceWorker not registered... ', err);
-      });
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('sw.js').then(function (register) {
+      console.log('ServiceWorker registered!');
+    }, function (err) {
+      console.log('ServiceWorker not registered... ', err);
     });
-  }
+  });
+}
